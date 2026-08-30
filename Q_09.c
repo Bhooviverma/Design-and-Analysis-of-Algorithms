9c46bd9088fe167036498d0e1ea0cff329230c25
Bhoovi Verma <bhooviverma02@gmail.com>
2026-08-30 12:42:04 +0530

    Add files via upload

---
+++ Q_09.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n, k;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    k = k % n;   // Handle k > n
+
+    // Left Rotate by k positions
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, n - 1);
+    reverse(arr, 0, n - 1);
+
+    printf("Array after left rotation:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
